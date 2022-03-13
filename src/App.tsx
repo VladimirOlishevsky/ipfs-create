c78797a952e88080e8a7ea5f4f94947f8626316d
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2022-03-13 17:29:57 +0400

    add test key

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ export const App = () => {
   const url = "https://api.pinata.cloud/pinning/pinFileToIPFS";
   const ref = useRef() as React.MutableRefObject<SignatureCanvas>;
 
+  const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2OGM3M2M4MS1iMDI4LTRiOTgtYWVkYS1kOWFjN2FlNGZlODciLCJlbWFpbCI6ImNvc3lzb2Z0d29ya0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlfSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiMjc0YjA0Yzg2YjIyNWJmOTdhOTQiLCJzY29wZWRLZXlTZWNyZXQiOiI5NDYxZTM1ZjIyMzkzZTZiNWNhZGM3YTRlOTQzNDc5YWVkMmYxZTE5OWIzYjA3YTFhZDkxYzQzM2NiNmUyMjViIiwiaWF0IjoxNjQ3MTc3ODE3fQ.9aQSkBUMwlMh9yqUtBIpitjBaWuA1IENA4YDkXhUOm4'
+
   const getData = async () => {
     const canvas = ref.current.getCanvas();
     const fd = new FormData();
@@ -22,7 +24,7 @@ export const App = () => {
         .post(url, fd, {
           maxContentLength: -1,
           headers: {
-            'Authorization': `Bearer ${process.env.REACT_APP_PINATA_JWT}`,
+            'Authorization': `Bearer ${apiKey}`,
           }
         });
       result.then((res) => {
@@ -38,7 +40,7 @@ export const App = () => {
       .get<IPinataListApi>(url2, {
         maxContentLength: -1,
         headers: {
-          'Authorization': `Bearer ${process.env.REACT_APP_PINATA_JWT}`,
+          'Authorization': `Bearer ${apiKey}`,
         }
       });
     result.then((res) => {
@@ -76,6 +78,7 @@ export const App = () => {
                 key={el}
                 className="img"
                 src={`https://gateway.pinata.cloud/ipfs/${el}?preview=1`}
+                // src={`https://ipfs.io/ipfs/${el}`}
                 alt="pinata-img" />
             )
           })}
